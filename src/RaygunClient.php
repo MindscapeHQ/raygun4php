e5c0532b0d4bac2f4fdf4eb3eb97978311c6dff2
Callum Gavin <callum@mindscape.co.nz>
2013-01-17 17:21:12 +1300

    Partial json post implementation using curl (WIP)

--- src/RaygunClient.php
+++ src/RaygunClient.php
@@ -23,6 +23,50 @@ namespace Raygun4php
 
         $message = new RaygunMessage();
         $message->Build($exception);
+
+        $json = json_encode($message);
+        echo $json, "</br>";
+
+        $httpData = curl_init('https://api.raygun.io/entries');
+        curl_setopt($httpData, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($httpData, CURLOPT_POST, 1);
+        curl_setopt($httpData, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($httpData, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($httpData, CURLINFO_HEADER_OUT, true);
+        curl_setopt($httpData, CURLOPT_SSL_VERIFYHOST, 2);
+        //curl_setopt($httpData, CURLOPT_CAPATH, getcwd().'/cert/');
+        curl_setopt($httpData, CURLOPT_CAINFO, realpath(__DIR__.'/api.raygun.io.crt'));
+        curl_setopt($httpData, CURLOPT_HTTPHEADER, array(
+            'X-ApiKey: '.$this->apiKey
+        ));
+
+        echo "about to send</br>";
+
+        $result = curl_exec($httpData);
+        if(curl_errno($httpData)){
+            echo 'Curl error: ' . curl_error($httpData);
+        }
+        echo $result;
+
+        echo curl_getinfo($httpData, CURLINFO_HEADER_OUT  ), "</br>";
+
+        echo "Http code returned: ", curl_getinfo($httpData, CURLINFO_HTTP_CODE);
+
+        //printf(curl_getinfo($httpData));
+        curl_close($httpData);
+
+        /*$content = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-Type: application/json' . "\r\n"
+                    . 'Content-Length: ' . strlen($json) . "\r\n"
+                    . 'X-ApiKey: ' .$this->apiKey ."\r\n",
+                'content' => $json,
+            ),
+        );
+
+        $result = file_get_contents("https://api.raygun.io/entries", true, stream_context_create($content));*/
+
     }
   }
 }
