233860c20edf15cef6c030c1d0d6045669b70578
Callum Gavin <callum@mindscape.co.nz>
2013-01-22 17:15:47 +1300

    Fixed names

--- src/Mindscape.Raygun4php/Raygun4PhpException.php
+++ src/Mindscape.Raygun4php/Raygun4PhpException.php
@@ -16,7 +16,9 @@
     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 */
-class Raygun4PhpException extends \Exception
+namespace Raygun4php
 {
-
+    class Raygun4PhpException extends \Exception
+    {
+    }
 }
