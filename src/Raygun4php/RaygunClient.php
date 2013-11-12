b75ae90f721bf0e15398dbbfbbabef91ec1449bf
steve.brazier <steve.brazier@vivomiles.com>
2013-11-12 09:15:39 +0000

    Removing all methods common to the message builder from the regular raygun client.

--- src/Raygun4php/RaygunClient.php
+++ src/Raygun4php/RaygunClient.php
@@ -5,22 +5,20 @@ namespace Raygun4php {
     require_once realpath(__DIR__ . '/Raygun4PhpException.php');
     require_once realpath(__DIR__ . '/Uuid.php');
 
-    use Rhumsaa\Uuid\Uuid;
-
     class RaygunClient
     {
         protected $apiKey;
-        protected $version;
-        protected $tags;
-        protected $user;
         protected $httpData;
         protected $useAsyncSending;
+
+        protected $messageBuilder;
 
         public function __construct($key, $useAsyncSending = true)
         {
             $this->apiKey = $key;
             $this->useAsyncSending = $useAsyncSending;
-            $this->SetUser();
+
+            $this->messageBuilder = new RaygunMessageBuilder();
         }
 
         /*
@@ -36,13 +34,13 @@ namespace Raygun4php {
         */
         public function SendError($errno, $errstr, $errfile, $errline, $tags = null, $userCustomData = null, $timestamp = null)
         {
-            $message = $this->BuildMessage(new \ErrorException($errstr, $errno, 0, $errfile, $errline), $timestamp);
+            $message = $this->messageBuilder->BuildMessage(new \ErrorException($errstr, $errno, 0, $errfile, $errline), $timestamp);
 
             if ($tags != null) {
-                $this->AddTags($message, $tags);
+                $this->messageBuilder->AddTagsToMessage($message, $tags);
             }
             if ($userCustomData != null) {
-                $this->AddUserCustomData($message, $userCustomData);
+                $this->messageBuilder->AddUserCustomDataToMessage($message, $userCustomData);
             }
 
             return $this->Send($message);
@@ -58,13 +56,13 @@ namespace Raygun4php {
         */
         public function SendException($exception, $tags = null, $userCustomData = null, $timestamp = null)
         {
-            $message = $this->BuildMessage($exception, $timestamp);
+            $message = $this->messageBuilder->BuildMessage($exception, $timestamp);
 
             if ($tags != null) {
-                $this->AddTags($message, $tags);
+                $this->messageBuilder->AddTagsToMessage($message, $tags);
             }
             if ($userCustomData != null) {
-                $this->AddUserCustomData($message, $userCustomData);
+                $this->messageBuilder->AddUserCustomDataToMessage($message, $userCustomData);
             }
 
             return $this->Send($message);
@@ -79,82 +77,9 @@ namespace Raygun4php {
          */
         public function SetVersion($version)
         {
-            $this->version = $version;
+            $this->messageBuilder->SetVersion($version);
         }
 
-        /*
-        *  Stores the current user of the calling application. This will be added to any messages sent
-        *  by this provider. It is used in the dashboard to provide unique user tracking.
-        *  If it is an email address, the user's Gravatar can be displayed. This method is optional,
-        *  if it is not used a random identifier will be assigned to the current user.
-        *  @param string $user A username, email address or other identifier for the current user
-        *  of the calling application.
-        *
-        */
-        public function SetUser($user = null)
-        {
-            if (is_string($user)) {
-                $this->user = $user;
-                if (php_sapi_name() != 'cli' && !headers_sent()) {
-                    setcookie('rguserid', $user, time() + 60 * 60 * 24 * 30);
-                    setcookie('rguuid', 'false', time() + 60 * 60 * 24 * 30);
-                }
-            } else {
-                if (!array_key_exists('rguuid', $_COOKIE)) {
-                    $this->user = (string)Uuid::uuid4();
-                    if (php_sapi_name() != 'cli' && !headers_sent()) {
-                        setcookie('rguserid', $this->user, time() + 60 * 60 * 24 * 30);
-                        setcookie('rguuid', 'true', time() + 60 * 60 * 24 * 30);
-                    }
-                } else {
-                    $this->user = $_COOKIE['rguserid'];
-                }
-            }
-        }
-
-        /*
-         * Sets a string array of tags relating to the message,
-         * used for identification. These will be transmitted along with messages that
-         * are sent.
-         * @param array $tags The tags relating to your project's version
-        */
-        private function BuildMessage($errorException, $timestamp = null)
-        {
-            $message = new RaygunMessage($timestamp);
-            $message->Build($errorException);
-            $message->Details->Version = $this->version;
-            $message->Details->Context = new RaygunIdentifier(session_id());
-
-            if ($this->user != null) {
-                $message->Details->User = new RaygunIdentifier($this->user);
-            } else {
-                $message->Details->User = new RaygunIdentifier($_COOKIE['rguserid']);
-            }
-            return $message;
-        }
-
-        private function AddTags(&$message, $tags)
-        {
-            if (is_array($tags)) {
-                $message->Details->Tags = $tags;
-            } else {
-                throw new \Raygun4php\Raygun4PhpException("Tags must be an array");
-            }
-        }
-
-        private function AddUserCustomData(&$message, $userCustomData)
-        {
-            if ($this->is_assoc($userCustomData)) {
-                $message->Details->UserCustomData = $userCustomData;
-            } else {
-                throw new \Raygun4php\Raygun4PhpException("UserCustomData must be an associative array");
-            }
-        }
-
-        private function is_assoc($array)
-        {
-            return (bool)count(array_filter(array_keys($array), 'is_string'));
-        }
 
         /*
          * Transmits an exception or ErrorException to the Raygun.io API. The default attempts to transmit asynchronously.
