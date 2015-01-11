bd9c635a9514cb708edab6383a0a5f8303eb7001
Andreas Möller <am@localheinz.com>
2015-01-11 19:07:45 +0100

    Fix: Remove require_once statements

--- src/Raygun4php/RaygunClient.php
+++ src/Raygun4php/RaygunClient.php
@@ -1,9 +1,5 @@
 <?php
 namespace Raygun4php {
-  require_once realpath(__DIR__ . '/RaygunMessage.php');
-  require_once realpath(__DIR__ . '/RaygunIdentifier.php');
-  require_once realpath(__DIR__ . '/Raygun4PhpException.php');
-  require_once realpath(__DIR__ . '/Uuid.php');
 
   use Raygun4Php\Rhumsaa\Uuid\Uuid;
 
