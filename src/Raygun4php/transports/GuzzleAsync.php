6bad0557493512fc4883faab8d0ad938cb9e903d
Viktor Hellström <anfly0@gmail.com>
2019-10-30 10:39:25 +0100

    Adds Guzzle async/sync transport implementations

---
+++ src/Raygun4php/transports/GuzzleAsync.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Raygun4php\Transports;
+use Raygun4php\Interfaces\RaygunMessageInterface;
+use Raygun4php\Interfaces\TransportInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\TransferException;
+use GuzzleHttp\Promise;
+
+class GuzzleAsync implements TransportInterface
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    private $httpPromises = [];
+
+    /**
+     * @param ClientInterface $httpClient
+     */
+    public function __construct(ClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function transmit(RaygunMessageInterface $message): bool
+    {
+        $messageJson = $message->toJson();
+
+        try {
+            $this->httpPromises[] = $this->httpClient
+                                 ->requestAsync('POST', '/entries', ['body' => $messageJson]);
+        } catch (TransferException $th) {
+            return false;
+        }
+        return true;
+    }
+
+    public function __destruct()
+    {
+        Promise\settle($this->httpPromises)->wait(false);
+    }
+}
---
+++ src/Raygun4php/transports/GuzzleSync.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Raygun4php\Transports;
+use Raygun4php\Interfaces\RaygunMessageInterface;
+use Raygun4php\Interfaces\TransportInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\TransferException;
+
+class GuzzleSync implements TransportInterface
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @param ClientInterface $httpClient
+     */
+    public function __construct(ClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function transmit(RaygunMessageInterface $message): bool
+    {
+        $messageJson = $message->toJson();
+
+        try {
+            $httpResponse = $this->httpClient
+                                 ->request('POST', '/entries', ['body' => $messageJson]);
+        } catch (TransferException $th) {
+            return false;
+        }
+
+        $responseCode = $httpResponse->getStatusCode();
+
+        return $responseCode === 202;
+    }
+}
