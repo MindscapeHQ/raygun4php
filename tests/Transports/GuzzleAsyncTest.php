607e165f35091915d8600989272ca95b72502f10
Viktor Hellström <anfly0@gmail.com>
2019-12-12 21:49:22 +0100

    Adds unit-tests for GuzzleAsync transport

---
+++ tests/Transports/GuzzleAsyncTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Raygun4php\Tests\Transports;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\Test\TestLogger;
+use Raygun4php\RaygunMessage;
+use Raygun4php\Transports\GuzzleAsync;
+
+class GuzzleAsyncTest extends TestCase
+{
+
+    public function testTransmitLogsErrorIfResponseCodeIs400()
+    {
+        $mock = new MockHandler([
+            new Response(400)
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $transport = new GuzzleAsync($client);
+        $message = new RaygunMessage();
+
+        $logger = new TestLogger();
+        $transport->setLogger($logger);
+
+        $transport->transmit($message);
+        $transport->__destruct();
+
+        $this->assertTrue($logger->hasErrorRecords());
+    }
+
+    public function testTransmitLogsWarningIfResponseCodeIs200()
+    {
+        $mock = new MockHandler([
+            new Response(200)
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $transport = new GuzzleAsync($client);
+        $message = new RaygunMessage();
+
+        $logger = new TestLogger();
+        $transport->setLogger($logger);
+
+        $transport->transmit($message);
+        $transport->__destruct();
+
+        $this->assertTrue($logger->hasWarningRecords());
+    }
+
+    public function testTransmitLogsErrorIfHttpClientThrowsException()
+    {
+        $mock = new MockHandler([
+            new ConnectException('Connection failed', new Request('POST', 'test'))
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $transport = new GuzzleAsync($client);
+        $message = new RaygunMessage();
+
+        $logger = new TestLogger();
+        $transport->setLogger($logger);
+
+        $transport->transmit($message);
+        $transport->__destruct();
+
+        $this->assertTrue($logger->hasErrorRecords());
+    }
+}
