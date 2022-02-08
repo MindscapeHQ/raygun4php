b49e9d8f9700671de0ff4e25a9d4b32bf463cc78
Ollie Bannister <olwiba@gmail.com>
2022-02-08 13:25:00 +1300

    Bumped the changelog

--- src/Raygun4php/RaygunClientMessage.php
+++ src/Raygun4php/RaygunClientMessage.php
@@ -11,7 +11,7 @@ class RaygunClientMessage
     public function __construct()
     {
         $this->Name = "Raygun4php";
-        $this->Version = "2.1.0";
+        $this->Version = "2.1.1";
         $this->ClientUrl = "https://github.com/MindscapeHQ/raygun4php";
     }
 }
