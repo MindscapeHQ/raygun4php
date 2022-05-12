6207977c4bed6dff9c13cee3bd2db36bba61ffae
Deacon <57920837+Deacon-McIntyre@users.noreply.github.com>
2022-05-13 10:48:44 +1200

    Bump version and add changelog entry

--- src/Raygun4php/RaygunClientMessage.php
+++ src/Raygun4php/RaygunClientMessage.php
@@ -11,7 +11,7 @@ class RaygunClientMessage
     public function __construct()
     {
         $this->Name = "Raygun4php";
-        $this->Version = "2.1.1";
+        $this->Version = "2.2.0";
         $this->ClientUrl = "https://github.com/MindscapeHQ/raygun4php";
     }
 }
